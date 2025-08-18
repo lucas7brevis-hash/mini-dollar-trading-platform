7474e77bd85adc875bbda62aa8923dbe9c6065d8
Mini Dollar Developer <developer@minidollar.com>
2025-08-18 19:45:49 -0400

    Adjust build.sh to ensure module path is correct

--- build.sh
+++ build.sh
@@ -7,19 +7,19 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 # Download NLTK data for sentiment analysis
-python -c "import nltk; nltk.download('punkt'); nltk.download('vader_lexicon')"
+python -c "import nltk; nltk.download(\'punkt\'); nltk.download(\'vader_lexicon\')"
 
 # Create database tables
 python -c "
 import os
 import sys
-sys.path.insert(0, '.')
+sys.path.insert(0, os.getcwd())
 from src.main import app
 from src.models.user import db
 from src.models.financial_data import CurrencyData, NewsData, TradingSignal
 
 with app.app_context():
     db.create_all()
-    print('Database tables created successfully!')
+    print(\'Database tables created successfully!\')
 "
 
