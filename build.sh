235b889686c30306e87a78113da6ed6f53f3befd
Mini Dollar Developer <developer@minidollar.com>
2025-08-18 20:24:41 -0400

    Add build-essential and ensure NLTK download in build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 # exit on error
 set -o errexit
 
-# Install dependencies
+# Install build dependencies
+sudo apt-get update && sudo apt-get install -y build-essential
+
+# Install Python dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
 
